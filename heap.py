b746804b56b8584249e24941ca28bb55977947e7
yepeng <initsysctrl@163.com>
2021-02-14 17:04:06 +0800

    add stack

---
+++ heap.py
@@ -0,0 +1,12 @@
+'''
+Date: 2021-02-14 17:01:24
+LastEditTime: 2021-02-14 17:01:54
+Author: Ye-P
+Descripttion: 堆
+'''
+
+
+class Heap(object):
+    def __init__(self) -> None:
+        super().__init__()
+        self.data = []
--- strack.py
+++ strack.py
@@ -1,14 +1,42 @@
 '''
 Date: 2021-02-14 01:17:07
 Author: Ye-P
-Descripttion: 
+Descripttion: 栈，先进后出 结构
 '''
 
-# 这是在test分支上弄的
-class Strack(object):
-    def __init__():
-        pass
+
+class Stack(object):
+    def __init__(self, limit=10):
+        self.data = []  # 栈容器
+        self.limit = limit  # 栈容器上限
+
+    def push(self, data):
+        if len(self.data) >= self.limit:
+            raise Exception('the overflow of stack.')
+        self.data.append(data)
+
+    def pop(self):
+        if not self.data:
+            raise IndexError('can not pop a empty stack.')
+        self.data.pop()
+
+    def size(self):
+        return len(self.data)
+
+    def is_empty(self):
+        return bool(self.data)
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.data[-1]
 
 
 if __name__ == '__main__':
-    pass
+    stack = Stack(limit=5)
+    for a in range(1, 6):
+        stack.push(a)
+        print(stack.data)
+    for a in range(1, 6):
+        stack.pop()
+        print(stack.data)
